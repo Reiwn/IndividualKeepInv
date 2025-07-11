aa73e5faee051c05579615577ea13f1137d8085c
uraioa <123806778+uraioa@users.noreply.github.com>
2025-07-11 08:20:42 +0000

    Update PlayerDeathMixin.java

--- src/main/java/rose/individualkeepinv/mixin/PlayerDeathMixin.java
+++ src/main/java/rose/individualkeepinv/mixin/PlayerDeathMixin.java
@@ -18,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
     @Inject(method = "dropInventory", at = @At("HEAD"), cancellable = true)
     public void onDropInventory(CallbackInfo info) {
         PlayerEntity player = ((PlayerEntity) (Object) this); // Getting the "this" instance in the PlayerEntity class
-            if (!player.getName().getString()=="NotDrop") {
+            if (player.getName().getString() != "NotDrop") {
                 // Not using the player variable here as the @Shadow annotation allows me to use the "this" instance directly
                 this.vanishCursedItems();
                 this.inventory.dropAll();
